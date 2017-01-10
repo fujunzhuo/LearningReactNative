f3420d95dd412c88fa13a8886301209b7347b18e
fujunzhuo <1134476937@qq.com>
2017-01-10 17:50:32 +0800

    'learning-react-native'

--- push.cmd
+++ push.cmd
@@ -2,3 +2,8 @@ git pull origin master
 git add -A
 git commit -m 'learning-react-native'
 git push origin master
+
+git pull origin master
+git add -A
+git commit -m 'learning-react-native'
+git push origin master
